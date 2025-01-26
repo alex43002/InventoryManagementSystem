4baa36dd7d86fe850775b353bd4cffd103de277b
Alexander DeJesus <alexdejesus336@yahoo.com>
2025-01-26 05:58:11 -0500

    Made the login page ui better

--- frontend-inventory-manage-system/src/app/features/login/login.component.ts
+++ frontend-inventory-manage-system/src/app/features/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
 export class LoginComponent {
   constructor(private authService: AuthService) {}
