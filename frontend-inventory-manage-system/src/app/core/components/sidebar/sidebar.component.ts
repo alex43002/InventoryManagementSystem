aa08c4340ea5873d365193f5c74b66241b0cc0c0
Alexander DeJesus <alexdejesus336@yahoo.com>
2025-01-27 02:50:13 -0500

    Rendering side bar correctly

--- frontend-inventory-manage-system/src/app/core/components/sidebar/sidebar.component.ts
+++ frontend-inventory-manage-system/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   standalone: true,
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatSidenavModule, MatListModule, CommonModule],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+    menuItems = [
+        { label: 'SKU CRUD', path: '/main/sku-crud' },
+        { label: 'Inventory', path: '/main/inventory' },
+        { label: 'Inventory CRUD', path: '/main/inventory-crud' },
+        { label: 'Reporting', path: '/main/reporting' },
+      ];
+
+    constructor(private router: Router) {}
+
+  navigate(link: string): void {
+    this.router.navigate([link]).catch((err) => console.error('Navigation Error:', err));
+  }
+}
