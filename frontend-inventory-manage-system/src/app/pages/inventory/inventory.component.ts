ff8b3637dd2f6ab8724fd9f8d343b7beca78ac37
Alexander DeJesus <alexdejesus336@yahoo.com>
2025-01-30 23:13:11 -0500

    Add initialization of component

--- frontend-inventory-manage-system/src/app/pages/inventory/inventory.component.ts
+++ frontend-inventory-manage-system/src/app/pages/inventory/inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inventory',
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
 })
-export class InventoryComponent {
-
+export class InventoryComponent implements OnInit{
   
+  
+  InventoryComponent(){
+    
+  }
+  
+  ngOnInit(): void {}
+
 }
