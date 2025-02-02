8dd1cfa2a823e3fe6848ccbaa78e4c6efb3aad5b
Alexander DeJesus <alexdejesus336@yahoo.com>
2025-02-02 08:48:43 -0500

    Template environment file

---
+++ frontend-inventory-manage-system/src/environments/environment.template.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    auth: {
+      domain: 'YOUR_AUTH0_DOMAIN',
+      clientId: 'YOUR_AUTH0_CLIENT_ID',
+      audience: 'YOUR_AUTH0_AUDIENCE'
+    },
+  };
+  
